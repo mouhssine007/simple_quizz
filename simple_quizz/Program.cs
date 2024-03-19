53c17318166089fcfd27f7593a3ce6fedda705d4
majdi mouhssine <majdimouhssine7@outlook.com>
2024-03-19 21:54:24 +0000

    Add project files.

---
+++ simple_quizz/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace simple_quizz
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] questions = new string[3]
+			{
+				"1. what is the capital of Morocco",
+				"2. what is the red planet?",
+				"3. what is the largest animal"
+			};
+			string[] answers = new string[3] { "Rabat", "Mars", "Whales" };
+			Console.WriteLine("Welcome user.");
+			Console.WriteLine("Please answer the following questions:");
+			for (int i = 0; i < questions.Length; i++) // Fixed the loop condition
+			{
+				Console.WriteLine(questions[i]);
+			
+				string userAnswer = Console.ReadLine();
+
+				try
+				{
+					bool result = IsCorrect(userAnswer, answers[i]);
+
+					if (result == true)
+					{
+						Console.WriteLine("Congratulations! Your answer is correct.");
+					}
+					else
+					{
+						Console.WriteLine($"Oops! The correct answer is {answers[i]}");
+					}
+				}
+				catch(Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			}
+			Console.WriteLine("quiee completed");
+		}
+
+		private static bool IsCorrect(string inputuser, string inputAnswer)
+		{
+			if (string.IsNullOrEmpty(inputuser))
+			{
+				throw new Exception("hey answer cannot be empty");
+			}
+
+			if (inputuser == inputAnswer)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+	}
+}
---
+++ simple_quizz/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("simple_quizz")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("simple_quizz")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c6e5ae3-9966-4fe3-9b9b-c56a3b03d07f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
